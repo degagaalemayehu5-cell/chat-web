be9d5b976f45b42ce38eec7369083e0b9f078452
Degaga Alemayehu <degagaalemayehu5@gmail.com>
2026-01-27 22:45:42 +0300

    Initial commit

---
+++ backend/src/routes/messages.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router ();
+
+
+router.get("/send" , (req,res) => {
+  res.send("send route");
+});
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+//const express = require('express');
+
+import express from "express";
+import dotenv from"dotenv";
+import authRoutes from "./routes/auth.js";
+import messagesRoutes from "./routes/messages.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use ("/api/auth",authRoutes);
+app.use ("/api/messages",messagesRoutes);
+
+app.listen (PORT, () => console.log ("server running on port :" + PORT));
