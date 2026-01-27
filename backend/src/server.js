f2d9787d73714e56d9ed113fe1972d6880bbfc71
Degaga Alemayehu <degagaalemayehu5@gmail.com>
2026-01-28 00:12:45 +0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,29 @@
 
 import express from "express";
 import dotenv from"dotenv";
+import Path from "path";
+
 import authRoutes from "./routes/auth.js";
 import messagesRoutes from "./routes/messages.js";
+
 
 dotenv.config();
 
 const app = express();
+const ___dirname =  Path.resolve ();
 
 const PORT = process.env.PORT || 3000;
 
 app.use ("/api/auth",authRoutes);
 app.use ("/api/messages",messagesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use (express.static (Path.join (___dirname, "../frontend/dist")));
+
+  app.get ("*", (req,res)=> {
+     res.sendFile (Path.join (___dirname, "../frontend/dist/index.html"));
+  });
+}
+  
 
 app.listen (PORT, () => console.log ("server running on port :" + PORT));
